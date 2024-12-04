4d742acc5491ff5ab65e0efb751e23d4e69191fa
Aditya Rawat <adityarawat313@gmail.com>
2024-12-04 17:45:29 +0530

    update readme

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -17,7 +17,7 @@ const AdminLogin = () => {
   const handleLogin = async () => {
     try {
       const response = await axios.post(
-        "https://enent-aditya-rawat.netlify.app//api/login",
+        "https://entnt-assessment-backend.onrender.com/api/login",
         formData
       );
       localStorage.setItem("token", response.data.token);
